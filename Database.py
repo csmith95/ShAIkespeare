2f4da0c759ab7f1a6de7c60d5e9321eb4ff86dcd
csmithy <csmith95@stanford.edu>
2017-11-15 16:34:32 -0800

    generate poem done

--- Database.py
+++ Database.py
@@ -1,16 +1,26 @@
-import re
+import re, random
 
 class Database:
 
   def __init__(self, filename):
-  	startTag = 'CONTENT/'
-  	endTag = '/CONTENT'
+  	self.corpus = []
+  	startTag = r'CONTENT/'
+  	endTag = r'/CONTENT'
+  	startAgeTag = r'AGE/'
+  	endAgeTag = r'/AGE'
   	with open(filename) as f:
-  		text = re.sub(r'\n+', ' NEWLINE ', f.read())
-  		text = re.sub(r'\s+', ' ', text)
-  		text = re.sub(r'\[.+\]', '', text)
-  		text = re.sub(r'[^a-zA-z\s\/]', '', text)
-  		matches = re.findall(r'{}(.*?){}'.format(startTag, endTag), text)
-  		self.corpus = [m.split() for m in matches]
+  		text = re.sub(r'\n+', ' NEWLINE ', f.read())	# encode newline as special token
+  		text = re.sub(r'\s+', ' ', text)				# clean up whitespace
+  		text = re.sub(r'\[.+?\]', '', text)				# strip [CHORUS] and other song tags
+  		text = re.sub(r'[,\.]', '', text) 	# remove some non-alphanumeric chars
+  		matches = re.findall(r'({})(.+?)({}).*?{}(.*?){}'.format(startAgeTag, endAgeTag, startTag, endTag), text)
+  		n = 0
+  		for m in matches:
+  			if m[1] == 'Renaissance':
+  				if random.randrange(1, 101) < 60: 
+  					continue
+  				n += 1
+  			self.corpus.append(m[3].split() + ['EOF'])
+
 
     
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from Database import Database
 
 db = Database('poem_song_db')
 bigram = BigramModel(db.corpus)
-print(bigram.generatePoem(40))
+print(bigram.generatePoem())
