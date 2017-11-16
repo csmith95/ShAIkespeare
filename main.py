cf790c0ec4ecc4ee0069498ac8de537e6f5b157c
Thomas Jensen <thomasjensen@DN0a1f63dd.SUNet>
2017-11-16 10:54:07 -0800

    Added rhyme function to return boolean if two input words rhyme

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from BigramModel import BigramModel
 from Database import Database
 from pyrhyme.rhyme import rhymes_with
+from pyrhyme.rhyme import rhymes
 
-
-# words = rhymes_with('space')
-# for word in words:
+#examples of the two rhyme functions
+# for word in rhymes_with('space'):
 #     print word
+# print(rhymes('space', 'lace'))
 
 db = Database('poem_song_db')
 bigram = BigramModel(db.corpus)
