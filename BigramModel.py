c6bf64342f63a27fe862a85d7d7d886fddfb3047
Thomas Jensen <thomasjensen@DN51usrq.SUNet>
2017-12-04 22:27:13 -0800

    added eval script in test.py

--- BigramModel.py
+++ BigramModel.py
@@ -175,38 +175,3 @@ def weightedRandomChoice(weightDict):
             chosenIndex = i
             return elems[chosenIndex]
     return 'EOF'
-
-# Function: Evaluate Poem
-# --------------------------------
-# Given a generated poem, evaluates it based on percentage of lines that rhyme
-# and percentage of lines that maintain the desired meter. Returns a tuple with
-# the two percentages.
-#
-# @ n -> the meter Ex. for iambicpentameter, n = 5
-# @ meterThreshold -> the weight that the line needs to generate to be considered
-#                     the desired meter. Should be around .6
-#
-def evalPoem(poemLines, n, meterThreshold):
-    A = ''
-    B = ''
-    numIambics = 0
-    numRhymes = 0
-    for i, line in enumerate(poemLines):
-        if iambic(line,n) >= meterThreshold: numIambics+=1
-        words = line.split(' ')
-        if i%2 == 0:
-            if i == 0:
-                A = words[-1]
-            else:
-                if A in rhymes_with(words[-1]) or words[-1] == A: numRhymes += 1
-                A = words[-1]
-        if i%2 == 1:
-            if i == 1:
-                B = words[-1]
-            else:
-                if B in rhymes_with(words[-1]) or words[-1] == B: numRhymes += 1
-                B = words[-1]
-    numLines = len(poemLines)
-    percentIambic = float(numIambics)/numLines
-    percentRhyme = float(numRhymes)/(numLines-2)
-    return (percentIambic,percentRhyme)
--- test.py
+++ test.py
@@ -1,32 +1,82 @@
 import prosodic as p
-# from pyrhyme.rhyme import rhymes_with
-# from nltk.corpus import wordnet
+from BigramModel import BigramModel
+from Database import Database
+from pyrhyme.rhyme import rhymes_with
 
-# def syllableCount(line):
-#     text = p.Text(line)
-#     text.parse()
-#     return len(text.syllables())
+
+
+def syllableCount(line):
+    text = p.Text(line)
+    text.parse()
+    return len(text.syllables())
+
+def iambic(line, n):
+    w = 1
+    if abs(syllableCount(line) - n*2) >= 1:
+        return .1
+    t = p.Text(line)
+    for i, syl in enumerate(t.syllables()):
+        syl.feature()
+        if i%2 == 1:
+            if syl.feature('prom.stress') != 1.0: w*=.9
+        if i%2 == 0:
+            if syl.feature('prom.stress') != 0.0: w*=.9
+    # if w >= .6: return True
+    # return False
+    return w
+
+# Function: Evaluate Poem
+# --------------------------------
+# Given a generated poem, evaluates it based on percentage of lines that rhyme
+# and percentage of lines that maintain the desired meter. Returns a tuple with
+# the two percentages.
 #
-# def iambic(line, n):
-#     w = 1
-#     if abs(syllableCount(line) - n*2) >= 1:
-#         return .1
-#     t = p.Text(line)
-#     for i, syl in enumerate(t.syllables()):
-#         syl.feature()
-#         if i%2 == 1:
-#             if syl.feature('prom.stress') != 1.0: w*=.9
-#         if i%2 == 0:
-#             if syl.feature('prom.stress') != 0.0: w*=.9
-#     # if w >= .6: return True
-#     # return False
-#     return w
+# @ n -> the meter Ex. for iambicpentameter, n = 5
+# @ meterThreshold -> the weight that the line needs to generate to be considered
+#                     the desired meter. Should be around .6
 #
-# poem = ["Was this the face that launched a thousand see",
-#         "But, soft! what light through yonder window breaks?",
-#         "And I do love thee: therefore, go with me",
-#         "Now is the winter of our discontent",
-#         "Which would be planted newly with the time",
-#         "As calling home our exiled friends abroad",
-#         "So, thanks to all at once and to each one",
-#         "abroad"]
+def evalPoem(poemLines, n, meterThreshold):
+    A = ''
+    B = ''
+    numIambics = 0
+    numRhymes = 0
+    for i, line in enumerate(poemLines):
+        if iambic(line,n) >= meterThreshold: numIambics+=1
+        words = line.split(' ')
+        if i%2 == 0:
+            if i == 0:
+                A = words[-1]
+            else:
+                if A in rhymes_with(words[-1]) or words[-1] == A: numRhymes += 1
+                A = words[-1]
+        if i%2 == 1:
+            if i == 1:
+                B = words[-1]
+            else:
+                if B in rhymes_with(words[-1]) or words[-1] == B: numRhymes += 1
+                B = words[-1]
+    numLines = len(poemLines)
+    percentIambic = float(numIambics)/numLines
+    percentRhyme = float(numRhymes)/(numLines-2)
+    return (percentIambic,percentRhyme)
+
+
+db = Database('poem_song_db')
+bigram = BigramModel(db.corpus)
+n = 1
+while(n != 0):
+    numPoems = 0
+    sumPercentIambic = 0
+    sumPercentRhyme = 0
+    for _ in range(n):
+        poem = bigram.generateConstrainedPoem()
+        tuple = evalPoem(poem, 5, .6)
+        sumPercentIambic += tuple[0]
+        sumPercentRhyme += tuple[1]
+
+    print "---------Evaluation----------"
+    print "Number of poems generated: ", n
+    print "Percent of lines that rhyme: ", float(sumPercentRhyme)/n
+    print "Percent of lines that have iambicpentameter: ", float(sumPercentIambic)/n
+    print "-----------------------------"
+    n = input("Enter number of poems to generate and evaluate (0 to quit): ")
